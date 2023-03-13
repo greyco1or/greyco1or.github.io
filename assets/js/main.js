a166e6682cae4963d390b33c1e02a33cce7393de
Grey Kim <starbux630@naver.com>
2023-03-13 09:32:22 +0900

    fin1

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,24 +23,32 @@ function blog_posts() {
     // keeping it static, can be fetched from a blog dynamically as well
     let posts = [
         {
-            url: 'https://www.nagekar.com/2017/02/trip-to-bramhatal-uttarakhand.html',
-            title: 'Trek To Bramhatal (Uttarakhand)',
+            url: 'https://blog.naver.com/starbux630/222905405694',
+            title: '[CS] 양방향 암호화(대칭키, 비대칭키)',
         },
         {
-            url: 'https://www.nagekar.com/2017/08/privacy.html',
-            title: 'Privacy - How I Converted',
+            url: 'https://blog.naver.com/starbux630/222929851935',
+            title: '[Solidity] 컨트랙트의 불변성, 소유(ownable), 함수제어자, 가스(가스비 최적화)',
         },
         {
             url: 'https://www.nagekar.com/2018/01/jagriti-yatra.html',
-            title: 'Jagriti Yatra 2017',
+            title: '[Solidity] 매핑, msg.sender, require, 상속, storage와 memory, internal과 external, 인터페이스, 다수의 리턴',
         },
         {
-            url: 'https://www.nagekar.com/2017/08/private-cloud-part-2.html',
-            title: 'Private Cloud Part 2 | Encrypted Storage With NextCloud',
+            url: 'https://blog.naver.com/starbux630/223006232231',
+            title: '[Java] 반복문 - for문, 향상된 for문, Listlterator',
         },
         {
-            url: 'https://www.nagekar.com/2018/07/eli5-how-https-works.html',
-            title: 'ELI5 - How HTTPS Works',
+            url: 'https://blog.naver.com/starbux630/222907941901',
+            title: '[블록체인] 비트코인의 거래 원리, 블록구조(블록해더), 채굴, SHA-256, 머클트리(머클루트)',
+        },
+        {
+            url: 'https://blog.naver.com/starbux630/223028386071',
+            title: '[블록체인] 노드와 지갑, 니모닉 - 비트코인',
+        },
+        {
+            url: 'https://blog.naver.com/starbux630/223035047319',
+            title: '[블록체인] Account 구조, 이더리움의 Tries - 이더리움',
         },
     ];
 
